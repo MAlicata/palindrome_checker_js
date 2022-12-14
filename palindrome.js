bbf0fb93857ea4ab71eb8cb6d7167080ddf8d5d4
MAlicata <matiasalicata@gmail.com>
2022-12-14 17:21:12 -0300

    add new features in palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,22 @@
 console.log("let's go");
 
-let word = "Matias ALICATA";
+let word = "ana";
 
 word = word.toLowerCase();
 word = word.replace(/[^a-z0-9]/g, '');
 
 let backward = word.split('');
 backward = backward.reverse().join('').toString();
+let flag;
+backward === word? flag = true : flag = false;
+console.log(flag);
 
-if (backward === word) {
+/*if (backward === word) {
     console.log("true");
     return true;
 } else {
     console.log("false");
     return false;
-}
+}*/
+
+
