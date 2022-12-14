486d2884215be9085c3274e5855737cdae183d7f
MAlicata <matiasalicata@gmail.com>
2022-12-14 17:03:46 -0300

    add update file palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,17 @@
 console.log("let's go");
 
-let words = "Race car";
+let word = "Matias ALICATA";
 
-// 1) remove spaces from string
-console.log(words.split(" ").join(""));
+word = word.toLowerCase();
+word = word.replace(/[^a-z0-9]/g, '');
+
+let backward = word.split('');
+backward = backward.reverse().join('').toString();
+
+if (backward === word) {
+    console.log("true");
+    return true;
+} else {
+    console.log("true");
+    return false;
+}
